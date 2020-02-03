f6d50a33e08cb20eeb71f41884a80cac2a4dd727
hyeoncheol kim <46736845+hyeoncheolkim91@users.noreply.github.com>
2020-02-03 17:59:38 -0600

    Add files via upload

---
+++ parse.js
@@ -0,0 +1,24 @@
+var parseString = require('xml2js').parseString;
+
+const fs = require('fs');
+
+const xml = fs.readFileSync("BH_Progress_Assessment.xml",'utf8');
+  //parseing xml to json object.
+  parseString(xml,{explicitArray: false, attrkey:"attr",charkey:"textData"}, function(err,result){
+      //Last property Array
+
+
+
+
+      //total 19 controls in this xml.
+      //You can choose each controls by changing index Object[i] located second element from the last.
+      var controlAttribute =result["Viklele.FormDesigner"].Object.Property[16].Object[0].Property[3].Object[0].attr;
+      var controlProperty = result["Viklele.FormDesigner"].Object.Property[16].Object[0].Property[3].Object[0].Property;
+
+    console.log(controlAttribute);
+    console.log(controlProperty);
+
+
+    // const result = JSON.stringify(result); <--- make it to string to save it into json file.
+    // fs.writeFileSynce("Assessment.json", result) <--- writing into json file.
+  });
