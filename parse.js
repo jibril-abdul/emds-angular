c647cbb2907431aa0c3e1c60d5b189d90a9f547d
hyeoncheol kim <46736845+hyeoncheolkim91@users.noreply.github.com>
2020-02-03 19:52:46 -0600

    Update parse.js

--- parse.js
+++ parse.js
@@ -1,24 +1,36 @@
 var parseString = require('xml2js').parseString;
-//To add package, use command 'npm install xml2js'
+
 const fs = require('fs');
 
 const xml = fs.readFileSync("BH_Progress_Assessment.xml",'utf8');
-  //parsing xml to json object.
-  parseString(xml,{explicitArray: false, attrkey:"attr",charkey:"textData"}, function(err,result){
-    
+  //parseing xml to json object.
+  parseString(xml,{explicitArray: false, charkey:"innerXML", explicitCharkey:true, mergeAttrs:true}, function(err,result){
+      
+
+      // var test = result["Viklele.FormDesigner"].Object;
 
 
 
 
-      //total 19 Controls in this xml.
-      //You can select each controls by changing index Object[i] located second element from the last.
-      var controlAttribute =result["Viklele.FormDesigner"].Object.Property[16].Object[0].Property[3].Object[0].attr;
-      var controlProperty = result["Viklele.FormDesigner"].Object.Property[16].Object[0].Property[3].Object[0].Property;
 
-    console.log(controlAttribute);
-    console.log(controlProperty);
+      //controls
+      var controlList = result["Viklele.FormDesigner"].Object.Property[16];
+
+      //You can choose each controls by changing index Object[i] located second element from the last.
+      var controlUseWaitCursor = result["Viklele.FormDesigner"].Object.Property[16].Object[0].Property[3].Object[0].Property[5];
+
+      var controlTabIndex = result["Viklele.FormDesigner"].Object.Property[16].Object[0].Property[3].Object[0].Property[4];
+
+      var controlName = result["Viklele.FormDesigner"].Object.Property[16].Object[0].Property[3].Object[0].Property[0];
+
+      var controlLocation = result["Viklele.FormDesigner"].Object.Property[16].Object[0].Property[3].Object[0].Property[2];
+
+      var controlSize = result["Viklele.FormDesigner"].Object.Property[16].Object[0].Property[3].Object[0].Property[3];
 
 
-    // const result = JSON.stringify(result); <--- make it to string to save it into json file.
-    // fs.writeFileSynce("Assessment.json", result) <--- writing into json file.
+      //printing controls
+    console.log(controlList);
+
+    // const resultToString = JSON.stringify(result);  //make it to string to save it into json file.
+    // fs.writeFileSync("Assessment.json", resultToString) // writing into json file.
   });
