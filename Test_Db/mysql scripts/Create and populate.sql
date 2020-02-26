2feafe2689a2d65f6fcef5e153b8f2a4852f6545
Dragofat <abrain47@gmail.com>
2020-02-25 20:37:27 -0600

    Updated create sql script

--- Test_Db/mysql scripts/Create and populate.sql
+++ Test_Db/mysql scripts/Create and populate.sql
@@ -2,6 +2,9 @@ create database if not exists test;
 
 use test;
 
+create user 'test'@'localhost' identified by 'password';
+grant all on test.* to 'test'@'localhost';
+
 show tables;
 
 drop table if exists `xmlhtmltable`;
