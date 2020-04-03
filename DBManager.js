f1da5cfcc2db36e02f1f8e154dc00d35cc6f08f1
hyeoncheol kim <hyeoncheolkim91@gmail.com>
2020-04-03 02:18:20 -0500

    Modularized

---
+++ DBManager.js
@@ -0,0 +1,24 @@
+const mysql = require('mysql');
+
+let pool = mysql.createPool({
+  connectionLimit: 10, // default = 10
+  host: 'localhost',
+  user: 'root',
+  password: 'password',
+  database: 'assessment'
+});
+
+module.exports.select = (paraName, callback) => {
+  let sql = "SELECT xmldata FROM xmlhtmltable WHERE Name = ?";
+  pool.query(sql, paraName, (err, result, fields) => {
+    if (err) {
+      console.log(err);
+    } else {
+      let xml = result[0].xmldata;
+      if (typeof callback == "function") {
+        callback(xml);
+      }
+
+    }
+  });
+};
